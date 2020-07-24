07927a09e8dffef1837cc83f425cb0f49245bb3c
selpeapi <selpeapi@gmail.com>
2020-07-24 23:55:47 +0900

    Initial commit

---
+++ bin/Dart_pra0.dart
@@ -0,0 +1,5 @@
+import 'package:Dart_pra0/Dart_pra0.dart' as Dart_pra0;
+
+void main(List<String> arguments) {
+  print('Hello world: ${Dart_pra0.calculate()}!');
+}
---
+++ test/Dart_pra0_test.dart
@@ -0,0 +1,8 @@
+import 'package:Dart_pra0/Dart_pra0.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
